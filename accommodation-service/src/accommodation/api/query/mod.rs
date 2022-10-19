6013ebb45b207a69e09b2b0b53144f8ae2d3fecc
Christian Schaible <cschaible@users.noreply.github.com>
2022-10-19 22:43:33 +0200

    Remove SampleQuery from graphql api in accommodation-service

--- accommodation-service/src/accommodation/api/query/mod.rs
+++ accommodation-service/src/accommodation/api/query/mod.rs
@@ -1,5 +1,4 @@
 use async_graphql::MergedObject;
-use async_graphql::Object;
 
 use crate::accommodation::api::query::resolvers::AccommodationResolver;
 
@@ -7,15 +6,5 @@ pub mod loaders;
 pub mod resolvers;
 pub mod types;
 
-#[derive(Default)]
-pub struct SampleQuery;
-
-#[Object]
-impl SampleQuery {
-    pub async fn howdy(&self) -> &'static str {
-        "partner"
-    }
-}
-
 #[derive(MergedObject, Default)]
-pub struct Query(SampleQuery, AccommodationResolver);
+pub struct Query(AccommodationResolver);
