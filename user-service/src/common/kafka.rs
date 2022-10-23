a3535bc9fa885998a26830d3aeb9f246cecb3e9c
Christian Schaible <cschaible@users.noreply.github.com>
2022-10-23 18:28:05 +0200

    Simplify avro encoder configuration in user-service

--- user-service/src/common/kafka.rs
+++ user-service/src/common/kafka.rs
@@ -8,13 +8,9 @@ pub fn init_avro_encoder<'a, 'b>(
     config: &'a KafkaConfiguration,
 ) -> Result<AvroEncoder<'b>, AppError> {
     let sr_settings = resolve_sr_settings(config)?;
-    Ok(get_avro_encoder(&sr_settings))
+    Ok(AvroEncoder::new(sr_settings))
 }
 
 fn resolve_sr_settings(config: &KafkaConfiguration) -> Result<SrSettings, AppError> {
     Ok(SrSettings::new_builder(config.schema_registry.url.clone()).build()?)
-}
-
-fn get_avro_encoder<'a>(sr_settings: &SrSettings) -> AvroEncoder<'a> {
-    AvroEncoder::new(sr_settings.clone())
 }
