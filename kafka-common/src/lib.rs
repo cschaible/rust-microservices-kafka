aceba46ef8d0721451b3eeeb912813d6af094861
Christian Schaible <cschaible@users.noreply.github.com>
2022-11-01 19:08:06 +0100

    Remove unused kafka related code from kafka-common

--- kafka-common/src/lib.rs
+++ kafka-common/src/lib.rs
@@ -3,8 +3,6 @@ use std::io::Cursor;
 use murmur3::murmur3_32;
 use uuid::Uuid;
 
-pub mod schema;
-
 pub fn partition_of(identifier: Uuid, num_partitions: i32) -> std::io::Result<i32> {
     Ok(
         murmur3_32(&mut Cursor::new(identifier.as_bytes()), 0)?.rem_euclid(num_partitions as u32)
