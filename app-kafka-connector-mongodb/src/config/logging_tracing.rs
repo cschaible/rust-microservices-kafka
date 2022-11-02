d14465655c2f28d11a3f816ee41f03d9ad3ab3d1
Christian Schaible <cschaible@users.noreply.github.com>
2022-11-02 22:00:50 +0100

    Apply root log level correctly

--- app-kafka-connector-mongodb/src/config/logging_tracing.rs
+++ app-kafka-connector-mongodb/src/config/logging_tracing.rs
@@ -8,10 +8,17 @@ pub fn init(config: &Configuration) -> Result<(), AppError> {
         env!("CARGO_PKG_NAME"),
         env!("CARGO_PKG_VERSION"),
         |mut e| {
+            // Configure root level
+            if let Some(root_level) = &config.logging.level.root {
+                e = e.add_directive(root_level.parse().unwrap_or_default())
+            }
+
+            // Configure specific directives
             for directive in &config.logging.level.directives {
                 let directive_string = format!("{}={}", directive.namespace, directive.level);
                 e = e.add_directive(directive_string.parse().unwrap_or_default());
             }
+
             e
         },
     )?)
